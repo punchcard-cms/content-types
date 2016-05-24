9f4d6eda25fb6e5d984ee2ebcfcae28066f37dd8
Sam Richard <sam@snug.ug>
2016-05-24 07:59:29 -0400

    :crystal_ball: Break Tests to Prove Point

--- tests/content-types.js
+++ tests/content-types.js
@@ -4,44 +4,31 @@ import types from '../lib/content-types';
 import util from 'util';
 import fs from 'fs';
 
-const fooContentObj = {
-  'name': 'FooRific',
-  'description': 'A very foo content model.',
-  'attributes': [
-    {
-      'type': 'email',
-      'id': 'email',
-      'name': 'Email',
-      'description': 'Your email is your username',
-    },
-  ],
-};
+// test('Content Types', t => {
+//   return types.only('bar', {
+//     'my-textarea': {
+//       textarea: {
+//         value: 'Hello World I am here'
+//       }
+//     }
+//   }).then(result => {
+//     // console.log(util.inspect(result, false, null));
+//     // fs.writeFileSync('fixtures/output.js', JSON.stringify(result, null, 2));
+//     t.pass();
+//   })
+// })
 
-test('Content Types', t => {
-  return types.only('bar', {
-    'my-textarea': {
-      textarea: {
-        value: 'Hello World I am here'
-      }
-    }
-  }).then(result => {
-    // console.log(util.inspect(result, false, null));
-    // fs.writeFileSync('fixtures/output.js', JSON.stringify(result, null, 2));
-    t.pass();
-  })
-})
-
-test('merged', t => {
-  return types()
-    .then(result => {
-      t.is(result[0].name, 'Content Type BAR', 'Get first content type name');
-      t.is(result[0].description, 'Bar Baz Foo', 'Get first content type desc');
-      t.is(result[0].id, 'bar', 'Get first content type id');
-      t.is(result[1].name, 'Content Type FOO', 'Get second content type name');
-      t.is(result[1].description, 'Foo Bar Baz', 'Get second content type desc');
-      t.is(result[1].id, 'foo', 'Get second content type id');
-    });
-});
+// test('merged', t => {
+//   return types()
+//     .then(result => {
+//       t.is(result[0].name, 'Content Type BAR', 'Get first content type name');
+//       t.is(result[0].description, 'Bar Baz Foo', 'Get first content type desc');
+//       t.is(result[0].id, 'bar', 'Get first content type id');
+//       t.is(result[1].name, 'Content Type FOO', 'Get second content type name');
+//       t.is(result[1].description, 'Foo Bar Baz', 'Get second content type desc');
+//       t.is(result[1].id, 'foo', 'Get second content type id');
+//     });
+// });
 
 // waiting on rejection story
 // test('merged with bad param', t => {
@@ -53,9 +40,34 @@ test('merged', t => {
 // })
 
 test('merged with correct param', t => {
-  fooContentObj.id = 'foo-rific';
-  return types([fooContentObj])
+  const testCT = {
+    name: 'FooRific',
+    description: 'A very foo content model.',
+    id: 'foo-rific',
+    attributes: [
+      {
+        type: 'text',
+        id: 'username',
+        name: 'Username',
+        description: 'Please enter a username',
+      },
+      {
+        type: 'text',
+        id: 'full-name',
+        name: 'Full Name',
+        description: 'Please enter your full name',
+        inputs: {
+          text: {
+            label: 'Foo Bar Baz',
+          },
+        },
+      },
+    ],
+  };
+
+  return types([testCT])
     .then(result => {
+      // console.log(util.inspect(result, false, null));
       t.is(result[0].name, 'FooRific', 'Get first content type name');
       t.is(result[0].description, 'A very foo content model.', 'Get first content type desc');
       t.is(result[0].id, 'foo-rific', 'Get first content type id');
