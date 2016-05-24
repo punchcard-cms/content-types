4b507068ac417db7c6eabb8d7f5073437f2f17d7
Sam Richard <sam@snug.ug>
2016-05-24 07:59:29 -0400

    :white_check_mark: Restore Tests

--- tests/content-types.js
+++ tests/content-types.js
@@ -4,31 +4,31 @@ import types from '../lib/content-types';
 import util from 'util';
 import fs from 'fs';
 
-// test('Content Types', t => {
-//   return types.only('bar', {
-//     'my-textarea': {
-//       textarea: {
-//         value: 'Hello World I am here'
-//       }
-//     }
-//   }).then(result => {
-//     // console.log(util.inspect(result, false, null));
-//     // fs.writeFileSync('fixtures/output.js', JSON.stringify(result, null, 2));
-//     t.pass();
-//   })
-// })
+test('Content Types', t => {
+  return types.only('bar', {
+    'my-textarea': {
+      textarea: {
+        value: 'Hello World I am here'
+      }
+    }
+  }).then(result => {
+    // console.log(util.inspect(result, false, null));
+    // fs.writeFileSync('fixtures/output.js', JSON.stringify(result, null, 2));
+    t.pass();
+  })
+})
 
-// test('merged', t => {
-//   return types()
-//     .then(result => {
-//       t.is(result[0].name, 'Content Type BAR', 'Get first content type name');
-//       t.is(result[0].description, 'Bar Baz Foo', 'Get first content type desc');
-//       t.is(result[0].id, 'bar', 'Get first content type id');
-//       t.is(result[1].name, 'Content Type FOO', 'Get second content type name');
-//       t.is(result[1].description, 'Foo Bar Baz', 'Get second content type desc');
-//       t.is(result[1].id, 'foo', 'Get second content type id');
-//     });
-// });
+test('merged', t => {
+  return types()
+    .then(result => {
+      t.is(result[0].name, 'Content Type BAR', 'Get first content type name');
+      t.is(result[0].description, 'Bar Baz Foo', 'Get first content type desc');
+      t.is(result[0].id, 'bar', 'Get first content type id');
+      t.is(result[1].name, 'Content Type FOO', 'Get second content type name');
+      t.is(result[1].description, 'Foo Bar Baz', 'Get second content type desc');
+      t.is(result[1].id, 'foo', 'Get second content type id');
+    });
+});
 
 // waiting on rejection story
 // test('merged with bad param', t => {
