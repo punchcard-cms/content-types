4f880711bfe9a363bc591a611196aeafbbc8c56b
Scott Nath <snath@us.ibm.com>
2016-06-22 11:29:29 -0400

    :white_check_mark: search for numbers too; update fixture

--- tests/fixtures/input-plugins/input-plugin-multiple-select.js
+++ tests/fixtures/input-plugins/input-plugin-multiple-select.js
@@ -39,12 +39,43 @@ module.exports = {
         empty: false,
       },
     },
-    puppyNameSelector: {
+    puppyNameSelector1: {
       validation: {
         function: 'puppyNameSelectorValidation',
         on: 'blur',
       },
-      label: 'Choose a puppy name',
+      label: 'Choose a puppy name version 1',
+      options: [
+        { label: 'Tinky Winky',
+          value: 'tink',
+        },
+        { label: 'Dipsy',
+          value: 'dip',
+        },
+        { label: 'Laa-Laa',
+          value: 'la',
+        },
+        { label: 'Po',
+          value: 'po',
+        },
+        { label: 'Brownie',
+          value: 'bro',
+        },
+        { label: 'Kevin',
+          value: 'kev',
+        },
+      ],
+      type: 'select',
+      settings: {
+        empty: false,
+      },
+    },
+    puppyNameSelector2: {
+      validation: {
+        function: 'puppyNameSelectorValidation',
+        on: 'blur',
+      },
+      label: 'Choose a puppy name version 2',
       options: [
         { label: 'Tinky Winky',
           value: 'tink',
@@ -71,5 +102,10 @@ module.exports = {
       },
     },
   },
-  html: '<label for="{{kittenNameSelector.id}}">{{kittenNameSelector.label}}</label><select id="{{kittenNameSelector.id}}" name="{{kittenNameSelector.name}}">{% for option in select.options %}<option value="{{option.value}}" {% if option.value == select.value %}selected{% endif %}>{{option.label}}</option>{% endfor %}</select></label><label for="{{puppyNameSelector.id}}">{{puppyNameSelector.label}}</label><select id="{{puppyNameSelector.id}}" name="{{puppyNameSelector.name}}">{% for option in select.options %}<option value="{{option.value}}" {% if option.value == select.value %}selected{% endif %}>{{option.label}}</option>{% endfor %}</select></label>',
+    html: `<label for="{{kittenNameSelector.id}}">{{kittenNameSelector.label}}</label>
+      <select id="{{kittenNameSelector.id}}" name="{{kittenNameSelector.name}}">{% for option in select.options %}<option value="{{option.value}}" {% if option.value == select.value %}selected{% endif %}>{{option.label}}</option>{% endfor %}</select>
+      <label for="{{puppyNameSelector1.id}}">{{puppyNameSelector1.label}}</label>
+      <select id="{{puppyNameSelector1.id}}" name="{{puppyNameSelector1.name}}">{% for option in select.options %}<option value="{{option.value}}" {% if option.value == select.value %}selected{% endif %}>{{option.label}}</option>{% endfor %}</select>
+      <label for="{{puppyNameSelector2.id}}">{{puppyNameSelector2.label}}</label>
+      <select id="{{puppyNameSelector2.id}}" name="{{puppyNameSelector2.name}}">{% for option in select.options %}<option value="{{option.value}}" {% if option.value == select.value %}selected{% endif %}>{{option.label}}</option>{% endfor %}</select>`,
 };
