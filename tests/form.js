30e3c788021143b384bbc7aa495b108466134877
Ayush Gupta <ayush2k@gmail.com>
2016-07-14 11:11:40 -0400

    :bug: changes delete input to button

--- tests/form.js
+++ tests/form.js
@@ -328,7 +328,6 @@ test('Form Generation, with repeatables', t => {
     };
   return form(input).then(rendered => {
     // fs.writeFileSync('fixtures/output.js', rendered.html);
-    t.true(includes(rendered.html, '<input type="button" value="add" class="add-button" id="my-quote-add">'), 'Contains Add button');
     t.true(includes(rendered.html, '<label for="c4087332-edda-432d-8464-cc9679742e4e--0">Citation:</label>'), 'Indexing for label of first instance');
     t.true(includes(rendered.html, '<input type="text" id="c4087332-edda-432d-8464-cc9679742e4e--0" name="my-quote--quote--0" value="foo" placeholder="Source Material" />'), 'Indexing for label of second instance');
     t.true(includes(rendered.html, '<label for="c4087332-edda-432d-8464-cc9679742e4e--1">Citation:</label>'), 'Indexing for label of second instance');
