1dc4b69efc6698fdc599d980fce862d69341e318
Scott Nath <snath@us.ibm.com>
2016-07-29 18:03:29 -0400

    :art: identifier checks abound

---
+++ tests/content-types-utils.js
@@ -0,0 +1,359 @@
+import test from 'ava';
+import cloneDeep from 'lodash/cloneDeep';
+import utils from '../lib/content-types/utils';
+
+const correct = {
+  name: 'Foo Bar Baz',
+  id: 'foo-bar-baz',
+  identifier: 'name',
+  attributes: [
+    {
+      type: 'text',
+      id: 'text',
+      name: 'Text',
+    },
+    {
+      type: 'email',
+      id: 'email',
+      name: 'Email',
+      repeatable: true,
+    },
+    {
+      type: 'text',
+      id: 'name',
+      name: 'Name',
+      inputs: {
+        text: {
+          required: 'save',
+        },
+        textarea: {
+          required: 'publish',
+        },
+      },
+    },
+  ],
+};
+
+
+//////////////////////////////
+// check content-type main configuration
+//////////////////////////////
+test('configuration check - object', t => {
+  const type = 'foo';
+
+  const result = utils.check.type(type);
+  t.is(result, 'Type must be an object', 'Should be an object');
+});
+
+test('configuration check - name', t => {
+  const type = {};
+
+  let result = utils.check.type(type);
+  t.is(result, 'Content types require a name', 'Should have a name');
+
+  type.name = '';
+  result = utils.check.type(type);
+  t.is(result, 'Content types require a name', 'Should have a name');
+});
+
+test('configuration check - name/string', t => {
+  const type = {
+    name: 123,
+  };
+
+  let result = utils.check.type(type);
+  t.is(result, 'Content type name must be string', 'Name should be a string');
+
+  type.name = [];
+  result = utils.check.type(type);
+  t.is(result, 'Content type name must be string', 'Name should be a string');
+});
+
+test('configuration check - id', t => {
+  const type = {
+    name: 'foo',
+  };
+
+  let result = utils.check.type(type);
+  t.is(result, 'Content types require an id', 'Should have an id');
+
+  type.id = '';
+  result = utils.check.type(type);
+  t.is(result, 'Content types require an id', 'Should have an id');
+});
+
+test('configuration check - id/string', t => {
+  const type = {
+    name: 'foo',
+    id: 123,
+  };
+
+  let result = utils.check.type(type);
+  t.is(result, 'Content type id must be string', 'ID should be a string');
+
+  type.id = [];
+  result = utils.check.type(type);
+  t.is(result, 'Content type id must be string', 'ID should be a string');
+});
+
+test('configuration check - id/kebab', t => {
+  const type = {
+    name: 'foo',
+    id: 'FooFoo',
+  };
+
+  const result = utils.check.type(type);
+  t.is(result, 'FooFoo needs to be written in kebab case (e.g. foofoo)', 'ID should be kebab');
+});
+
+test('Content Type config check identifier', t => {
+  const type = cloneDeep(correct);
+  type.identifier = [];
+  const check = utils.check.type(type);
+  t.is(check, 'Identifier in content type \'Foo Bar Baz\' must be a string', 'identifier must be a string');
+});
+
+test('Content Type config check identifier exists', t => {
+  const type = cloneDeep(correct);
+  type.identifier = 'sandwich';
+  const check = utils.check.type(type);
+  t.is(check, 'Identifier \'sandwich\' is not an attribute in content type \'Foo Bar Baz\'.', 'identifier must be a string');
+});
+
+test('Content Type config check identifier exists', t => {
+  const type = cloneDeep(correct);
+  type.identifier = 'name';
+  const check = utils.check.type(type);
+  t.is(check, 'Attribute \'name\' in content type \'Foo Bar Baz\' cannot be the identifier. Only attributes with one input can be the identifier. Duh.', 'identifier must be a string');
+});
+
+test('Content Type config uses attributes check', t => {
+  const type = cloneDeep(correct);
+  type.identifier = 'name';
+  delete type.attributes;
+  const check = utils.check.type(type);
+  t.is(check, 'Content type \'Foo Bar Baz\' must have attributes', 'identifier must be a string');
+});
+
+
+//////////////////////////////
+// check content-type attributes configuration
+//////////////////////////////
+test('Content Type check attributes exists', t => {
+  const type = {
+    name: 'Foo',
+    id: 'foo',
+  };
+  const check = utils.check.attributes(type);
+  t.is(check, 'Content type \'Foo\' must have attributes', 'A Content Type must have attributes');
+});
+
+test('Content Type attributes is an array', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: 'attributes',
+  };
+  const check = utils.check.attributes(type);
+  t.is(check, 'Attributes in content type \'test\' must be an array', 'Content Type attributes must be an array');
+});
+
+test('Content Type - at least one attribute', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [],
+  };
+  const check = utils.check.attributes(type);
+  t.is(check, 'Content type \'test\' must have at least one attribute', 'Content Type should have at least one attribute');
+});
+
+test('Content Type config check attribute requires name', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{}],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'undefined\' in content type \'test\' needs a name', 'Content Type attributes require a name');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: '',
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'undefined\' in content type \'test\' needs a name', 'Content Type attributes require a name');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: '',
+    id: 'bar',
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'bar\' in content type \'test\' needs a name', 'Content Type attributes require a name');
+});
+
+test('Content Type config attribute name/string', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{
+      name: ['foo'],
+    }],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute names in content type \'test\' must be a string', 'Content Type attributes require a name');
+
+  type.attributes = [{
+    name: [],
+  }, {
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: true,
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute names in content type \'test\' must be a string', 'Content Type attributes require a name');
+});
+
+test('Content Type config check attribute requires id', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{
+      name: 'foo',
+      id: '',
+    }, {
+      name: 'bar',
+    }],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'foo\' in content type \'test\' must have an id', 'Content Type attributes require a name');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: 'bar',
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'bar\' in content type \'test\' must have an id', 'Content Type attributes require a name');
+});
+
+test('Content Type config check attribute id is string', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{
+      name: 'foo',
+      id: [],
+    }, {
+      name: 'bar',
+    }],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute id in \'foo\' in content type \'test\' must be a string', 'Attribute id must be a string');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: 'bar',
+    id: {},
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute id in \'bar\' in content type \'test\' must be a string', 'Attribute id must be a string');
+});
+
+test('Content Type config check attribute requires type', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{
+      name: 'foo',
+      id: 'foo',
+      type: '',
+    }, {
+      name: 'bar',
+    }],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'foo\' in content type \'test\' must have a type', 'Content Type attributes require a type');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: 'bar',
+    id: 'bar',
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute \'bar\' in content type \'test\' must have a type', 'Content Type attributes require a type');
+});
+
+test('Content Type config check attribute type is string', t => {
+  const type = {
+    name: 'test',
+    id: 'test',
+    attributes: [{
+      name: 'foo',
+      id: 'foo',
+      type: [],
+    }, {
+      name: 'bar',
+      id: 'bar',
+    }],
+  };
+  let check = utils.check.attributes(type);
+  t.is(check, 'Attribute type in \'foo\' in content type \'test\' must be a string.', 'Attribute type must be a string');
+
+  type.attributes = [{
+    name: 'foo',
+    id: 'foo',
+    type: 'text',
+  }, {
+    name: 'bar',
+    id: 'bar',
+    type: {},
+  }];
+  check = utils.check.attributes(type);
+  t.is(check, 'Attribute type in \'bar\' in content type \'test\' must be a string.', 'Attribute type must be a string');
+});
+
+test('configuration check - winner', t => {
+  const type = {
+    name: 'Foo',
+    id: 'foo',
+    identifier: 'name',
+    attributes: [
+      {
+        type: 'text',
+        id: 'text',
+        name: 'Text',
+      },
+      {
+        type: 'email',
+        id: 'email',
+        name: 'Email',
+      },
+      {
+        type: 'text',
+        id: 'name',
+        name: 'Name',
+      },
+    ],
+  };
+
+  const result = utils.check.type(type);
+  t.true(result, 'Should be happy with a proper content type config');
+});
--- tests/content-types.js
+++ tests/content-types.js
@@ -21,19 +21,18 @@ test('Content Types', t => {
 test('merged', t => {
   return types()
     .then(result => {
-      console.log(JSON.stringify(result[2], null, 2));
       t.is(result[0].name, 'Content Type BAR', 'Get first content type name');
       t.is(result[0].description, 'Bar Baz Foo', 'Get first content type desc');
       t.is(result[0].id, 'bar', 'Get first content type id');
-      t.is(result[0].key, 'something-new', 'Get first content type key');
+      t.is(result[0].identifier, 'something-new', 'Get first content type identifier');
       t.is(result[1].name, 'Content Type Baz', 'Get second content type name');
       t.is(result[1].description, 'Bar Baz Foo', 'Get second content type desc');
       t.is(result[1].id, 'baz', 'Get second content type id');
-      t.is(result[1].key, 'plugin-required-save', 'Get second content type key');
+      t.is(result[1].identifier, 'plugin-required-save', 'Get second content type identifier');
       t.is(result[2].name, 'Content Type FOO', 'Get third content type name');
       t.is(result[2].description, 'Foo Bar Baz', 'Get third content type desc');
       t.is(result[2].id, 'foo', 'Get third content type id');
-      t.is(result[2].key, 'new-text-thing', 'Get third content type key');
+      t.is(result[2].identifier, 'new-text-thing', 'Get third content type identifier');
     });
 });
 
@@ -57,6 +56,39 @@ test('reject when something other than array is passed in', t => {
   });
 });
 
+test('reject when same attribute ids', t => {
+  const testCT = {
+    name: 'Foo',
+    id: 'foo',
+    attributes: [
+      {
+        type: 'text',
+        id: 'text',
+        name: 'Text',
+      },
+      {
+        type: 'text',
+        id: 'text',
+        name: 'Text',
+      },
+    ],
+  };
+
+  return types([testCT]).then(() => {
+    t.fail('Merged should fail');
+  }).catch(e => {
+    t.is(e.message, 'Input ID \'text\' in content type \'Foo\' cannot be duplicated (in \'Text\')', 'Dupe ids Rejected');
+  });
+});
+
+test('reject when content type config has issues', t => {
+  return types([{}]).then(() => {
+    t.fail('Merged should fail');
+  }).catch(e => {
+    t.is(e.message, 'Content types require a name', 'Triggers config check');
+  });
+});
+
 test('reject when plugin not found', t => {
   const testCT = [{
     name: 'Foo',
@@ -77,13 +109,16 @@ test('reject when plugin not found', t => {
   });
 });
 
-test('reject when name not found - no id', t => {
+test('reject when identifier is mutli-input', t => {
   const testCT = [{
     name: 'Foo',
     id: 'foo',
+    identifier: 'quote',
     attributes: [
       {
-        type: 'text',
+        type: 'quote',
+        id: 'quote',
+        name: 'Quote',
       },
     ],
   }];
@@ -91,78 +126,20 @@ test('reject when name not found - no id', t => {
   return types(testCT).then(() => {
     t.fail('Merged should fail');
   }).catch(e => {
-    t.is(e.message, 'Input \'text\' in content type \'Foo\' needs a name', 'Missing Name');
+    t.is(e.message, 'Identifier quote in content type \'Foo\' has more than one input. Only single-input attributes may be the identifier.', 'Identifier only allowed one input');
   });
 });
 
-test('reject when name not found - id', t => {
+test('reject when identifier is repeatable', t => {
   const testCT = [{
     name: 'Foo',
     id: 'foo',
+    identifier: 'text',
     attributes: [
       {
         type: 'text',
-        id: 'My Text',
-      },
-    ],
-  }];
-
-  return types(testCT).then(() => {
-    t.fail('Merged should fail');
-  }).catch(e => {
-    t.is(e.message, 'Input \'My Text\' in content type \'Foo\' needs a name', 'Missing Name');
-  });
-});
-
-test('reject when id not found', t => {
-  const testCT = [{
-    name: 'Foo',
-    id: 'foo',
-    attributes: [
-      {
-        type: 'text',
-        name: 'My Text',
-      },
-    ],
-  }];
-
-  return types(testCT).then(() => {
-    t.fail('Merged should fail');
-  }).catch(e => {
-    t.is(e.message, 'Input \'My Text\' in content type \'Foo\' needs an ID', 'Missing ID');
-  });
-});
-
-test('reject when id is not kebab case', t => {
-  const testCT = [{
-    name: 'Foo',
-    id: 'foo',
-    attributes: [
-      {
-        type: 'text',
-        name: 'My Text',
-        id: 'myText',
-      },
-    ],
-  }];
-
-  return types(testCT).then(() => {
-    t.fail('Merged should fail');
-  }).catch(e => {
-    t.is(e.message, 'Input ID \'myText\' needs to be written in kebab case (e.g. \'my-text\')', 'Not Kebab ID');
-  });
-});
-
-test('reject when key is repeatable', t => {
-  const testCT = [{
-    name: 'Foo',
-    id: 'foo',
-    attributes: [
-      {
-        type: 'text',
-        name: 'My Text',
-        id: 'my-text',
-        key: true,
+        id: 'text',
+        name: 'Text',
         repeatable: true,
       },
     ],
@@ -171,47 +148,19 @@ test('reject when key is repeatable', t => {
   return types(testCT).then(() => {
     t.fail('Merged should fail');
   }).catch(e => {
-    t.is(e.message, 'Repeatable attributes can not be the key. Input \'My Text\' in content type \'Foo\' is repeatable and has key: true.', 'Only single key');
+    t.is(e.message, 'Identifier text in content type \'Foo\' is repeatable. Only non-repeatable attributes may be the identifier.', 'Identifier cannot be repeatable');
   });
 });
 
-test('reject when more than one key', t => {
-  const testCT = [{
-    name: 'Foo',
-    id: 'foo',
-    attributes: [
-      {
-        type: 'text',
-        name: 'My Text',
-        id: 'my-text',
-        key: true,
-      },
-      {
-        type: 'text',
-        name: 'My Text2',
-        id: 'my-text-2',
-        key: true,
-      },
-    ],
-  }];
-
-  return types(testCT).then(() => {
-    t.fail('Merged should fail');
-  }).catch(e => {
-    t.is(e.message, 'Only one attribute can be the key. Input \'My Text2\' in content type \'Foo\' has the second key found.', 'Only single key');
-  });
-});
-
-test('reject when more than one input on key attribute', t => {
+test('reject when all identifiers are mutli-input', t => {
   const testCT = [{
     name: 'Foo',
     id: 'foo',
     attributes: [
       {
         type: 'quote',
-        name: 'My Quote',
-        id: 'my-quote',
-        key: true,
+        id: 'quote',
+        name: 'Quote',
       },
     ],
   }];
@@ -219,44 +168,20 @@ test('reject when more than one input on key attribute', t => {
   return types(testCT).then(() => {
     t.fail('Merged should fail');
   }).catch(e => {
-    t.is(e.message, 'Only attributes with one input can be the key. Plugin \'input-plugin-quote\' in content type \'Foo\' has more than one input.', 'Only single input');
+    t.is(e.message, 'Content type \'Foo\' does not have an attribute which can be an identifier.', 'Identifier only allowed one input');
   });
 });
 
-test('reject when more than one input on key attribute', t => {
-  const testCT = [{
-    name: 'FooKey',
-    id: 'foo-key',
-    attributes: [
-      {
-        type: 'quote',
-        name: 'My Quote',
-        id: 'my-quote',
-      },
-    ],
-  }];
-
-  return types(testCT).then(() => {
-    t.fail('Merged should fail');
-  }).catch(e => {
-    t.is(e.message, 'Content type \'FooKey\', does not have an attribute with one input that can be used as a key.', 'No attribute can be a key');
-  });
-});
-
-test('reject when id is duplicated', t => {
+test('reject when identifier is repeatable', t => {
   const testCT = [{
     name: 'Foo',
     id: 'foo',
     attributes: [
       {
         type: 'text',
-        name: 'My Text',
-        id: 'my-text',
-      },
-      {
-        type: 'email',
-        name: 'My Email',
-        id: 'my-text',
+        id: 'text',
+        name: 'Text',
+        repeatable: true,
       },
     ],
   }];
@@ -264,7 +189,7 @@ test('reject when id is duplicated', t => {
   return types(testCT).then(() => {
     t.fail('Merged should fail');
   }).catch(e => {
-    t.is(e.message, 'Input ID \'my-text\' in content type \'Foo\' cannot be duplicated (in \'My Email\')', 'No Duplicate IDs');
+    t.is(e.message, 'Content type \'Foo\' does not have an attribute which can be an identifier.', 'Identifier only allowed one input');
   });
 });
 
@@ -279,6 +204,7 @@ test('merged with correct param', t => {
         type: 'text',
         id: 'username',
         name: 'Username',
+        repeatable: {},
         description: 'Please enter a username',
       },
       {
@@ -319,7 +245,7 @@ test('merged with correct param', t => {
       t.is(merged.name, 'FooRific', 'Content type name does not change');
       t.is(merged.description, 'A very foo content model.', 'Content type description does not change');
       t.is(merged.id, 'foo-rific', 'Content type ID does not change');
-      t.is(merged.key, 'username', 'Content type key is automatically selected');
+      t.is(merged.identifier, 'full-name', 'Content type identifier is automatically selected');
       t.true(merged.hasOwnProperty('attributes'), 'Content type has attributes');
       t.is(merged.attributes.length, 3, 'Content type has three attributes');
 
@@ -425,299 +351,4 @@ test('get attributes of one type', t => {
       const attrs = types.raw.attributes('bar', result);
       t.is(result[0].attributes, attrs, 'Should get the attributes object of one type');
     });
-});
-
-//////////////////////////////
-// check content-type configuration
-//////////////////////////////
-test('configuration check - object', t => {
-  const type = 'foo';
-
-  const result = types.raw.check(type);
-  t.is(result, 'Type must be an object', 'Should be an object');
-});
-
-test('configuration check - name', t => {
-  const type = {};
-
-  let result = types.raw.check(type);
-  t.is(result, 'Content types require a name', 'Should have a name');
-
-  type.name = '';
-  result = types.raw.check(type);
-  t.is(result, 'Content types require a name', 'Should have a name');
-});
-
-test('configuration check - name/string', t => {
-  const type = {
-    name: 123,
-  };
-
-  let result = types.raw.check(type);
-  t.is(result, 'Content type name must be string', 'Name should be a string');
-
-  type.name = [];
-  result = types.raw.check(type);
-  t.is(result, 'Content type name must be string', 'Name should be a string');
-});
-
-test('configuration check - id', t => {
-  const type = {
-    name: 'foo',
-  };
-
-  let result = types.raw.check(type);
-  t.is(result, 'Content types require an id', 'Should have an id');
-
-  type.id = '';
-  result = types.raw.check(type);
-  t.is(result, 'Content types require an id', 'Should have an id');
-});
-
-test('configuration check - id/string', t => {
-  const type = {
-    name: 'foo',
-    id: 123,
-  };
-
-  let result = types.raw.check(type);
-  t.is(result, 'Content type id must be string', 'ID should be a string');
-
-  type.id = [];
-  result = types.raw.check(type);
-  t.is(result, 'Content type id must be string', 'ID should be a string');
-});
-
-test('configuration check - id/kebab', t => {
-  const type = {
-    name: 'foo',
-    id: 'FooFoo',
-  };
-
-  const result = types.raw.check(type);
-  t.is(result, 'FooFoo needs to be written in kebab case (e.g. foofoo)', 'ID should be kebab');
-});
-
-test('Content Type config check identifier', t => {
-  const type = {
-    name: 'foo',
-    id: 'foo',
-    identifier: 'true',
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Content type identifier must be a boolean', 'identifier must be a boolean');
-
-  type.identifier = '';
-  check = types.raw.check(type);
-  t.is(check, 'Content type identifier must be a boolean', 'identifier must be a boolean');
-});
-
-test('Content Type check attributes exists', t => {
-  const type = {
-    name: 'Foo',
-    id: 'foo',
-  };
-  const check = types.raw.check(type);
-  t.is(check, 'A content type must have attributes', 'A Content Type must have attributes');
-});
-
-test('Content Type attributes is an array', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: 'attributes',
-  };
-  const check = types.raw.check(type);
-  t.is(check, 'Content type attributes must be an array', 'Content Type attributes must be an array');
-});
-
-test('Content Type - at least one attribute', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [],
-  };
-  const check = types.raw.check(type);
-  t.is(check, 'Content type must have at least one attribute', 'Content Type should have at least one attribute');
-});
-
-test('Content Type config check attribute requires name', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{}],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute must have a name', 'Content Type attributes require a name');
-
-  type.attributes = [{
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: '',
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute must have a name', 'Content Type attributes require a name');
-});
-
-test('Content Type config attribute name/string', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{
-      name: ['foo'],
-    }],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute name must be a string', 'Content Type attributes require a name');
-
-  type.attributes = [{
-    name: [],
-  }, {
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: 'bar',
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute name must be a string', 'Content Type attributes require a name');
-});
-
-test('Content Type config check attribute requires id', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{
-      name: 'foo',
-      id: '',
-    }, {
-      name: 'bar',
-    }],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute must have an id', 'Content Type attributes require a name');
-
-  type.attributes = [{
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: 'bar',
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute must have an id', 'Content Type attributes require a name');
-});
-
-test('Content Type config check attribute id is string', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{
-      name: 'foo',
-      id: [],
-    }, {
-      name: 'bar',
-    }],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute id must be a string', 'Attribute id must be a string');
-
-  type.attributes = [{
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: 'bar',
-    id: {},
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute id must be a string', 'Attribute id must be a string');
-});
-
-test('Content Type config check attribute requires type', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{
-      name: 'foo',
-      id: 'foo',
-      type: '',
-    }, {
-      name: 'bar',
-    }],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute must have a type', 'Content Type attributes require a type');
-
-  type.attributes = [{
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: 'bar',
-    id: 'bar',
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute must have a type', 'Content Type attributes require a type');
-});
-
-test('Content Type config check attribute type is string', t => {
-  const type = {
-    name: 'test',
-    id: 'test',
-    attributes: [{
-      name: 'foo',
-      id: 'foo',
-      type: [],
-    }, {
-      name: 'bar',
-      id: 'bar',
-    }],
-  };
-  let check = types.raw.check(type);
-  t.is(check, 'Attribute type must be a string', 'Attribute type must be a string');
-
-  type.attributes = [{
-    name: 'foo',
-    id: 'foo',
-    type: 'text',
-  }, {
-    name: 'bar',
-    id: 'bar',
-    type: {},
-  }];
-  check = types.raw.check(type);
-  t.is(check, 'Attribute type must be a string', 'Attribute type must be a string');
-});
-
-
-
-test('configuration check - winner', t => {
-  const type = {
-    name: 'Foo',
-    id: 'foo',
-    identifier: true,
-    attributes: [
-      {
-        type: 'text',
-        id: 'text',
-        name: 'Text',
-      },
-      {
-        type: 'email',
-        id: 'email',
-        name: 'Email',
-      },
-      {
-        type: 'text',
-        id: 'name',
-        name: 'Name',
-      },
-    ],
-  };
-
-  const result = types.raw.check(type);
-  t.true(result, 'Should be happy with a proper content type config');
 });
