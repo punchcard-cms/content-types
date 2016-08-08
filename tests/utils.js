7896746f1377badf25fa7483e2fc0ef3a2330c56
Ayush Gupta <ayush2k@gmail.com>
2016-08-08 12:02:29 -0400

    :unamused: renaming reference util to utils

--- tests/utils.js
+++ tests/utils.js
@@ -1,21 +1,21 @@
 import test from 'ava';
-import util from '../lib/util.js';
+import utils from '../lib/utils.js';
 import _ from 'lodash';
 import path from 'path';
 
 test('Getplugins works with directory value as string', t => {
   const input = {};
   _.set(input, 'content.plugins.directory', path.join(process.cwd(), 'fixtures'));
-  t.is(typeof util.getPlugins(input), 'object');
+  t.is(typeof utils.getPlugins(input), 'object');
 });
 
 test('Getplugins works with directory value as boolean', t => {
   const input = {};
   _.set(input, 'content.plugins.directory', true);
-  t.is(typeof util.getPlugins(input), 'object');
+  t.is(typeof utils.getPlugins(input), 'object');
 });
 
 test('Getplugins works with directory value as undefined', t => {
   const input = {};
-  t.is(typeof util.getPlugins(input), 'object');
+  t.is(typeof utils.getPlugins(input), 'object');
 });
