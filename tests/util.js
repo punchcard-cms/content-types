1c965df2851499785355a0b8b5eb6af3745189ca
Ayush Gupta <ayush2k@gmail.com>
2016-08-08 11:33:29 -0400

    :white_check_mark: adds test for getPlugins

--- tests/util.js
+++ tests/util.js
@@ -3,13 +3,19 @@ import util from '../lib/util.js';
 import _ from 'lodash';
 import path from 'path';
 
-test('Getplugins works with string', t => {
+test('Getplugins works with directory value as string', t => {
   const input = {};
   _.set(input, 'content.plugins.directory', path.join(process.cwd(), 'fixtures'));
   t.is(typeof util.getPlugins(input), 'object');
 });
 
-test('Getplugins works with nothing', t => {
+test('Getplugins works with directory value as boolean', t => {
+  const input = {};
+  _.set(input, 'content.plugins.directory', true);
+  t.is(typeof util.getPlugins(input), 'object');
+});
+
+test('Getplugins works with directory value as undefined', t => {
   const input = {};
   t.is(typeof util.getPlugins(input), 'object');
 });
